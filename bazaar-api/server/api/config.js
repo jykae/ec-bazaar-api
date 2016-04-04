5e3c08f09c8c3a85a1102adeb3e1feffb17e81f1
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-04-04 14:11:05 +0300

    Initial file structuring

---
+++ bazaar-api/server/api/config.js
@@ -0,0 +1,18 @@
+// Global API configuration
+API = {};
+
+// Version 1
+API.v2 = new Restivus({
+  apiPath: 'api/',
+  version: 'v2',
+  defaultHeaders: {
+    'Content-Type': 'application/json'
+  },
+  useDefaultAuth: false,
+  prettyJson: true,
+  enableCors: true
+});
+
+// Add Restivus Swagger configuration
+// - meta, definitions, params, tags
+API.v1.swagger = {};
