cb9226b20f98d656f07221841a93b585aefa4359
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-04-05 16:52:17 +0300

    Add Swagger config

--- bazaar-api/server/api/config.js
+++ bazaar-api/server/api/config.js
@@ -1,3 +1,5 @@
+import { MaterialsSchema } from '/server/collections/schema';
+
 // Application namespace
 const Bazaar = {};
 
@@ -16,8 +18,58 @@ Bazaar.Api.v2 = new Restivus({
   enableCors: true
 });
 
-// Add Restivus Swagger configuration
-// - meta, definitions, params, tags
+// Init Restivus Swagger configuration
 Bazaar.Api.v2.swagger = {};
+
+// Add swagger meta
+Bazaar.Api.v2.swagger.meta = {
+  swagger: "2.0",
+  info: {
+    version: "2.0",
+    title: "EduCloud Bazaar API",
+    description: "EduCloud Bazaar REST API",
+    termsOfService: "https://dashboard.digipalvelutehdas.fi/terms/",
+    contact: {
+      name: "Ville Jyrkkä"
+    },
+    license: {
+      name: "MIT"
+    }
+  }
+};
+
+// Add swagger params
+Bazaar.Api.v2.swagger.params = {
+  materialId: {
+    name: "id",
+    in: "path",
+    description: "Material ID",
+    required: true,
+    type: "string"
+  },
+  material: {
+    name: "material",
+    in: "body",
+    description: "Material to add.",
+    required: true,
+    schema: {
+      $ref: "#/definitions/material"
+    }
+  }
+};
+
+// Add swagger tags
+Bazaar.Api.v2.swagger.tags = {
+  material: "Materials"
+};
+
+// Init swagger definitions
+Bazaar.Api.v2.swagger.definitions = {};
+
+// Extend definitions with MaterialsSchema definitions
+_.extend(Bazaar.Api.v2.swagger.definitions, MaterialsSchema.definitions);
+
+// Generate Swagger to route /api/v2/swagger.json
+Bazaar.Api.v2.addSwagger('swagger.json');
 
 export { Bazaar };
