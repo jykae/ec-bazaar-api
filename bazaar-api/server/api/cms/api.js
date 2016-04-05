0f63321df52c0275a6ffbec6a00cc24caf15527a
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-04-05 16:53:24 +0300

    Add swagger definitions

--- bazaar-api/server/api/cms/api.js
+++ bazaar-api/server/api/cms/api.js
@@ -3,4 +3,87 @@ import { Materials } from '/server/collections/materials';
 
 // Generates: GET, POST on /api/items and GET, PUT, DELETE on
 // /api/items/:id for the Items collection
-Bazaar.Api.v2.addCollection(Materials);
+Bazaar.Api.v2.addCollection(Materials, {
+  routeOptions: {
+    authRequired: false
+  },
+  endpoints: {
+    get: {
+      swagger: {
+        tags: [
+          Bazaar.Api.v2.swagger.params.material
+        ],
+        description: "Get one material with given ID.",
+        parameters: [
+          Bazaar.Api.v2.swagger.params.materialId
+        ],
+        responses: {
+          "200": {
+            description: "One material"
+          }
+        }
+      }
+    },
+    post: {
+      swagger: {
+        tags: [
+          Bazaar.Api.v2.swagger.params.material
+        ],
+        description: "Create a material.",
+        parameters: [
+          Bazaar.Api.v2.swagger.params.material
+        ],
+        responses: {
+          "200": {
+            description: "Return material that was added."
+          }
+        }
+      }
+    },
+    put: {
+      swagger: {
+        tags: [
+          Bazaar.Api.v2.swagger.params.material
+        ],
+        description: "Update material with given ID.",
+        parameters: [
+          Bazaar.Api.v2.swagger.params.materialId
+        ],
+        responses: {
+          "200": {
+            description: "Returns updated material."
+          }
+        }
+      }
+    },
+    delete: {
+      swagger: {
+        tags: [
+          Bazaar.Api.v2.swagger.params.material
+        ],
+        description: "Delete material with given ID.",
+        parameters: [
+          Bazaar.Api.v2.swagger.params.materialId
+        ],
+        responses: {
+          "200": {
+            description: "Successful deleted material with given ID."
+          }
+        }
+      }
+    },
+    getAll: {
+      swagger: {
+        tags: [
+          Bazaar.Api.v2.swagger.params.material
+        ],
+        description: "Returns authenticated user's materials.",
+        responses: {
+          "200": {
+            description: "List of materials."
+          }
+        }
+      }
+    }
+  }
+});
