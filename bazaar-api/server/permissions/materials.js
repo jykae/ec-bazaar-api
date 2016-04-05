e63d0d7a11e0caebacf945b3bc4798ba87e3b00c
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-04-05 16:58:43 +0300

    Add initial permissions

---
+++ bazaar-api/server/permissions/materials.js
@@ -0,0 +1,13 @@
+import { Materials } from '/server/collections/materials';
+
+Materials.allow({
+  insert: function () {
+    return true;
+  },
+  update: function () {
+    return true;
+  },
+  remove: function () {
+    return true;
+  }
+});
---
+++ bazaar-api/server/permissions/products.js
@@ -0,0 +1,15 @@
+/*
+import { Products } from '/server/collections/products';
+
+Products.allow({
+  insert: function () {
+    return true;
+  },
+  update: function () {
+    return true;
+  },
+  remove: function () {
+    return true;
+  }
+});
+*/
