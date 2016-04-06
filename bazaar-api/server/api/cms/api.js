f980809cebc039238d43a9fa27db55d0a8b2cc93
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-04-06 11:57:46 +0300

    Add metadata endpoints, fix params to tags.

--- bazaar-api/server/api/cms/api.js
+++ bazaar-api/server/api/cms/api.js
@@ -1,5 +1,6 @@
 import { Bazaar } from '/server/api/config';
 import { Materials } from '/server/collections/materials';
+import { Metadata } from '/server/collections/metadata';
 
 // Generates: GET, POST on /api/items and GET, PUT, DELETE on
 // /api/items/:id for the Items collection
@@ -11,7 +12,7 @@ Bazaar.Api.v2.addCollection(Materials, {
     get: {
       swagger: {
         tags: [
-          Bazaar.Api.v2.swagger.params.material
+          Bazaar.Api.v2.swagger.tags.material
         ],
         description: "Get one material with given ID.",
         parameters: [
@@ -27,7 +28,7 @@ Bazaar.Api.v2.addCollection(Materials, {
     post: {
       swagger: {
         tags: [
-          Bazaar.Api.v2.swagger.params.material
+          Bazaar.Api.v2.swagger.tags.material
         ],
         description: "Create a material.",
         parameters: [
@@ -43,7 +44,7 @@ Bazaar.Api.v2.addCollection(Materials, {
     put: {
       swagger: {
         tags: [
-          Bazaar.Api.v2.swagger.params.material
+          Bazaar.Api.v2.swagger.tags.material
         ],
         description: "Update material with given ID.",
         parameters: [
@@ -59,7 +60,7 @@ Bazaar.Api.v2.addCollection(Materials, {
     delete: {
       swagger: {
         tags: [
-          Bazaar.Api.v2.swagger.params.material
+          Bazaar.Api.v2.swagger.tags.material
         ],
         description: "Delete material with given ID.",
         parameters: [
@@ -75,7 +76,7 @@ Bazaar.Api.v2.addCollection(Materials, {
     getAll: {
       swagger: {
         tags: [
-          Bazaar.Api.v2.swagger.params.material
+          Bazaar.Api.v2.swagger.tags.material
         ],
         description: "Returns authenticated user's materials.",
         responses: {
@@ -84,7 +85,63 @@ Bazaar.Api.v2.addCollection(Materials, {
           }
         }
       }
-      // TODO: Override action, get user's materials 
+      // TODO: Override action, get user's materials
+    }
+  }
+});
+
+// GET ALL endpoint for Metadata
+Bazaar.Api.v2.addCollection(Metadata, {
+  routeOptions: {
+    authRequired: false
+  },
+  endpoints: {
+    getAll: {
+      swagger: {
+        tags: [
+          Bazaar.Api.v2.swagger.tags.material
+        ],
+        description: "Returns all available metadata.",
+        responses: {
+          "200": {
+            description: "List of metadata."
+          }
+        }
+      }
+    }
+  }
+});
+
+// GET metadata by country
+// Maps to: /api/v2/metadata/:country
+Bazaar.Api.v2.addRoute('metadata/:country', {authRequired: false}, {
+  get: {
+    swagger: {
+      tags: [
+        Bazaar.Api.v2.swagger.tags.material
+      ],
+      description: "Returns all available metadata by country.",
+      parameters: [
+        Bazaar.Api.v2.swagger.params.country
+      ],
+      responses: {
+        "200": {
+          description: "List of metadata by country parameter."
+        }
+      }
+    },
+    action: function () {
+      // Get organizationId from URL parameters
+      const countryCode = this.urlParams.country;
+      // Fetch all departments of the given organization
+      const metadata = Metadata.find({ "language": countryCode }).fetch();
+
+      // Build response
+      const response = {
+        status: "success",
+        data: metadata
+      }
+      return response;
     }
   }
 });
